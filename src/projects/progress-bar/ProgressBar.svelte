e3e2179a5c7a1be38aa97ef140747be2b6f03f41
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2021-04-03 23:05:24 +0200

    Completed msg on uploading done

--- src/projects/progress-bar/ProgressBar.svelte
+++ src/projects/progress-bar/ProgressBar.svelte
@@ -1,4 +1,6 @@
 <script>
+    import { fly } from 'svelte/transition';
+    
     let barWidth = 0;
     let progress;
     let isActive;
@@ -6,22 +8,37 @@
     $: if (barWidth === 100) {
         clearInterval(progress);
         isActive = false;
+        document.querySelector('#myFile').value = '';
+        setTimeout(() => {
+            resetProgress();
+        }, 3000);
     }
 
     const addWidth = () => barWidth += 1;
+
+    const resetProgress = () => {
+        barWidth = 0;
+        clearInterval(progress);
+    }
 
     const makeProgress = () => {
         if (!document.querySelector('#myFile').value) {
             alert('Please, choose a file!');
         } else {
             isActive = true;
-            progress = setInterval(addWidth, 10);
+            progress = setInterval(addWidth, 20);
         }
     }
 </script>
 
 <!-- MarkUp -->
 <h2>Progress Bar</h2>
+
+<div class="msg-container">
+    {#if barWidth === 100}
+    <h3 id="completedMsg" in:fly={{ x: -300}} out:fly={{ x: 300}}>Your file has been uploaded!</h3>
+    {/if}
+</div>
 
 <div id="myProgress">
     <span>{barWidth}%</span>
@@ -31,10 +48,26 @@
 <form on:submit|preventDefault={makeProgress}>
     <input type="file" id="myFile" name="filename">
     <button disabled={isActive}>Upload File</button>
+    <button on:click|preventDefault={resetProgress}>Cancel</button>
 </form>
 
 
 <style>
+    .msg-container {
+        text-align: center;
+        height: 3rem;
+        overflow: hidden;
+    }
+
+    #completedMsg {
+        color: coral;
+        animation: blink .75s ease-in-out 3;
+    }
+
+    @keyframes blink {
+        to {opacity: 0;}
+    }
+
 #myProgress {
   width: 100%;
   background-color: #ccc;
