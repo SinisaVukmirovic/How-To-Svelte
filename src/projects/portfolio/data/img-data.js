cffa297472ab987c26db818faeaec34b0b9d8e7f
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2021-04-13 20:57:29 +0200

    Added IDs to portfolio items

--- src/projects/portfolio/data/img-data.js
+++ src/projects/portfolio/data/img-data.js
@@ -1,35 +1,41 @@
 export const images = [
     {
+        id: 1,
         name: 'ANCIENT APPARITION',
         url: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/ancient_apparition_full.png',
         key: 'inteligence',
         desc: 'support, nuker, disabler'
     },
     {
+        id: 2,
         name: 'JUGGERNAUT',
         url: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/juggernaut_full.png',
         key: 'agility',
         desc: 'carry, escape, pusher'
     },
     {
+        id: 3,
         name: 'OMNIKNIGHT',
         url: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/omniknight_full.png',
         key: 'strength',
         desc: 'support, nuker, durable'
     },
     {
+        id: 4,
         name: 'CRYSTAL MAIDEN',
         url: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/crystal_maiden_full.png',
         key: 'inteligence',
         desc: 'support, nuker, disabler, jungler'
     },
     {
+        id: 5,
         name: 'SVEN',
         url: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/sven_full.png',
         key: 'strength',
         desc: 'carry, nuker, disabler, durable, initiator'
     },
     {
+        id: 6,
         name: 'MIRANA',
         url: 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/mirana_full.png',
         key: 'agility',
